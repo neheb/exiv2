ea93fc2f7c24bcdfb59b5dd6a16102e2c5248595
Rosen Penev <rosenp@gmail.com>
2025-03-30 16:13:48 -0700

    int

--- include/exiv2/config.h
+++ include/exiv2/config.h
@@ -3,14 +3,6 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-///// Start of Visual Studio Support /////
-#ifdef _MSC_VER
-
-#pragma warning(disable : 4996)  // Disable warnings about 'deprecated' standard functions
-
-#endif  // _MSC_VER
-///// End of Visual Studio Support /////
-
 #include "exv_conf.h"
 
 #endif  // _CONFIG_H_
